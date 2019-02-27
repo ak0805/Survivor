11a31fd3cb3a054206190388167fbb8f0adb4695
Michał <michalS237@wp.pl>
2019-02-27 20:07:12 +0100

    próbowałem cos zrobić ale nie umiem

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,9 @@
+package Main;
+
+import Window.NewWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        new NewWindow().setVisible(true);
+    }
+}
---
+++ src/main/java/Window/NewWindow.java
@@ -0,0 +1,25 @@
+package Window;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NewWindow extends JFrame {
+    private JPanel panel;
+    public NewWindow() {
+        super("Survivor Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        panel = new JPanel();
+        panel.setLayout(null);
+
+        add(panel);
+
+    }
+    public void PaintComponent(Graphics g){
+        super.paintComponents(g);
+        Graphics2D g2d =(Graphics2D) g;
+        g2d.drawRect(10,10,200,200);
+    }
+
+
+}
