fe80eadc8eae6054c48c94fd456938c7a71f117c
Grzegorz Pioro <gpioro2001@gmail.com>
2019-03-15 20:22:23 +0100

    Start window and game window

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,9 +1,14 @@
 package Main;
 
+import Window.GameWindow;
 import Window.NewWindow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         new NewWindow().setVisible(true);
+
     }
 }
--- src/main/java/Window/NewWindow.java
+++ src/main/java/Window/NewWindow.java
@@ -1,25 +1,36 @@
 package Window;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.*;
 
-public class NewWindow extends JFrame {
-    private JPanel panel;
+public class NewWindow extends JFrame implements ActionListener {
+    Canvas canvas = new Canvas();
+    GameWindow gameWindow = new GameWindow();
+
     public NewWindow() {
-        super("Survivor game");
+        super("Survivor Game");
+        canvas.setLayout(null);
+        JTextArea textField = new JTextArea();
+        textField.setBounds(235, 220, 200, 20);
+        JButton startButton = new JButton("START GAME");
+        startButton.setBounds(235, 250, 200, 50);
+        startButton.addActionListener(e -> {
+            setVisible(false);
+            gameWindow.setVisible(true);
+        });
+
+
+        setSize(654, 436);
+        setResizable(false);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-        panel = new JPanel();
-        panel.setLayout(null);
-
-        add(panel);
-
-    }
-    public void PaintComponent(Graphics g){
-        super.paintComponents(g);
-        Graphics2D g2d =(Graphics2D) g;
-        g2d.drawRect(10,10,200,200);
+        canvas.add(textField);
+        canvas.add(startButton);
+        add(canvas);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
+    }
 }
